2626c974e459c9ebf7d96837fd62ccb2986750da
netanel <netanelbenza@gmail.com>
2026-05-27 20:29:45 +0300

    completing main.py file (The project was completed)

---
+++ main.py
@@ -0,0 +1,12 @@
+from circle import Circle
+from square import Squre
+from triangle import Tringle
+from hexagon import Hexagon
+
+
+s1 = Squre(5)
+print(s1)
+s2 = Squre(10)
+print(s2)
+c1 = Circle(0)
+print(c1)
